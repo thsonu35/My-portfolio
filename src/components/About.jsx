c6561091066ce2750fcb51916b57a3072f756f50
Sohan Singh Thakur <85925090+thsonu35@users.noreply.github.com>
2024-12-30 11:02:11 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,12 +11,11 @@ const About = () => (
       <div className="about-container section">
         <h2 className="section-subtitle">My Journey</h2>
         <p className="about-text">
-          My journey into the world of web development started during my undergraduate studies in Computer Science. I was fascinated by the ability to build interactive and visually appealing websites, which led me to dive deep into web technologies. I have since honed my skills through various projects and internships.
-        </p>
-        <h2 className="section-subtitle">Internship Experience</h2>
-        <h4 className="internship-title">Web Development Intern at Indivibe Infotech</h4>
+My journey into the world of web development began during my undergraduate studies in Computer Science, where I discovered a passion for creating interactive and visually engaging web applications. Over the past five years, I have cultivated my expertise as a MERN Stack Developer, mastering technologies like ReactJS, Node.js, Express.js, and MongoDB. This experience spans diverse projects and professional roles, including designing scalable architectures, developing user-centric interfaces, and implementing secure backend systems. Each project has sharpened my technical skills and deepened my understanding of building dynamic, high-performance applications that deliver exceptional user experiences.        </p>
+        <h2 className="section-subtitle"> Experience</h2>
+        <h4 className="internship-title">Full Stack Development </h4>
         <p className="about-text">
-          During my internship at Indivibe Infotech, I had the opportunity to work on several exciting projects. I was responsible for:
+          I had the opportunity to work on several exciting projects. I was responsible for:
           <ul>
             <li>Developing User Interfaces: I collaborated with the design team to create user-friendly and visually appealing interfaces using React.js.</li>
             <li>API Integration: Integrated RESTful APIs to ensure seamless data flow between the frontend and backend.</li>
